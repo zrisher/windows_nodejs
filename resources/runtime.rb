4c391147db09d72d0c2396b6d3def52b68e143c3
Zach Risher <zrisher@yahoo.com>
2016-12-13 06:13:27 -0800

    continue

--- resources/runtime.rb
+++ resources/runtime.rb
@@ -3,7 +3,7 @@ provides :windows_nodejs_runtime, os: 'windows' if respond_to?(:provides)
 actions :create
 default_action :create
 
-attribute :version,  kind_of: String, name_attribute: true
+attribute :version,  kind_of: String
 attribute :source, kind_of: [String, nil], default: nil
 attribute :checksum, kind_of: [String, nil], default: nil
 

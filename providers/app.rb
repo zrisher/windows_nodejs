de9dc0cd2b07fe15023effd7f3416bb133625d53
Zach Risher <zach.risher@gmail.com>
2016-12-14 07:14:03 -0800

    continue

--- providers/app.rb
+++ providers/app.rb
@@ -5,28 +5,9 @@ def whyrun_supported?; true; end
 
 
 action :create do
-  Chef::Log.info "Create app: #{new_resource.name}"
+  Chef::Log.info "windows_nodejs::app:create name: #{new_resource.name}"
 end
 
-#
-# Author:: Conrad Kramer <conrad@kramerapps.com>
-# Cookbook Name:: application_node
-# Resource:: node
-#
-# Copyright:: 2013, Kramer Software Productions, LLC. <conrad@kramerapps.com>
-#
-# Licensed under the Apache License, Version 2.0 (the 'License');
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
 =begin
 include Chef::DSL::IncludeRecipe

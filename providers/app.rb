8419153a855582098a9ed2187fdc6889026e8b67
Zach Risher <zach.risher@gmail.com>
2016-12-14 12:42:27 -0800

    c

--- providers/app.rb
+++ providers/app.rb
@@ -15,7 +15,7 @@ action :create do
     comment                    'NodeJS Deploy Agent'
     home                       r.deploy_user_home
     shell                      r.deploy_user_shell
-    password                   SecureRandom.urlsafe_base64[0, 15]
+    password                   'asr32r2@!#$dfdEDFD' #SecureRandom.urlsafe_base64[0, 15]
     manage_home                true
   end
 
